9936ff70af4148f7576e86573184b2436ab76ef1
shawon39 <shshawon39@gmail.com>
2020-05-25 07:15:11 +0600

    Updates

---
+++ precache-manifest.ebb6e1f42639276ec633806ff2699fe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea77b9c5f8ff5f0eea077d7e4607b10e",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "c18fe36e8f85a7317c94",
+    "url": "/Tic-Tac-Toe-React/static/css/main.4654c282.chunk.css"
+  },
+  {
+    "revision": "e216e77d034d2f68b9a9",
+    "url": "/Tic-Tac-Toe-React/static/js/2.fbc3e0e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.fbc3e0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18fe36e8f85a7317c94",
+    "url": "/Tic-Tac-Toe-React/static/js/main.87c84b8a.chunk.js"
+  },
+  {
+    "revision": "cba97596f2a2df81ac67",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.ef4fa987.js"
+  }
+]);
